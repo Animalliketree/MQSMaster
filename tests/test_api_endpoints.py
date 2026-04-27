ca12d9d8da70ded60ac427e03fc93f58c88d8002
6lack-L <6lack@mbp.local>
2026-04-27 12:22:54 -0230

    update workflow to handle networking request faster in cicd

--- tests/test_api_endpoints.py
+++ tests/test_api_endpoints.py
@@ -1,6 +1,8 @@
 # tests/test_api_endpoints.py
-import pytest
 from datetime import datetime, timedelta
+
+import pytest
+
 from src.orchestrator.marketData.fmpMarketData import FMPMarketData
 
 pytestmark = [
@@ -9,14 +11,17 @@ pytestmark = [
     pytest.mark.workflow_nlp,
 ]
 
+
 @pytest.fixture
 def start_date():
-    start_date = (datetime.now() - timedelta(days=7))
+    start_date = datetime.now() - timedelta(days=7)
     return start_date.strftime("%Y-%m-%d")
+
 
 @pytest.fixture
 def end_date():
     return datetime.now().strftime("%Y-%m-%d")
+
 
 @pytest.fixture
 def fmp():
@@ -25,17 +30,25 @@ def fmp():
 
 def test_historical_data(fmp, start_date, end_date):
     data = fmp.get_historical_data("AAPL", start_date, end_date)
-    assert data is not None and isinstance(data, list) and len(data) > 0, "Historical data api failed"
+    assert data is not None and isinstance(data, list) and len(data) > 0, (
+        "Historical data api failed"
+    )
+
 
 def test_intraday_data(fmp, start_date, end_date):
     data = fmp.get_intraday_data("AAPL", start_date, end_date, 5)
-    assert data is not None and isinstance(data, list) and len(data) > 0, "Intraday data api failed"
+    assert data is not None and isinstance(data, list) and len(data) > 0, (
+        "Intraday data api failed"
+    )
+
 
 def test_realtime_data(fmp, start_date, end_date):
     data = fmp.get_realtime_data("NASDAQ")
-    assert data is not None and isinstance(data, list) and len(data) > 0, "Realtime data api failed"
+    assert data is not None and isinstance(data, list) and len(data) > 0, (
+        "Realtime data api failed"
+    )
+
 
 def test_curent_price(fmp):
     price = fmp.get_current_price("AAPL")
     assert isinstance(price, float) and price > 0, "Current Price api failed"
-
